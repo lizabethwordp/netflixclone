9a6fefe2f3b86a3f3dda6188c23fbc91d55c308e
lizabeth <52053436+lizabethwordp@users.noreply.github.com>
2024-07-01 14:39:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,41 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
-# Log in to ArgoCD (if not already logged in)
+# Download the Helm installation script
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+# Make the script executable
+chmod 700 get_helm.sh
+
+# Run the installation script
+./get_helm.sh
+
+helm repo add stable https://charts.helm.sh/stable
+
+helm repo update
+
+helm search repo stable
+
+helm install my-release stable/<chart-name>
+
+git clone https://github.com/lizabethwordp/netflixclone.git
+cd netflixclone
+
+helm create netflix-app
+
 argocd login <argocd-server-url> --username <username> --password <password>
 
-# Create an application
-argocd app create my-app \
-    --repo <git-repo-url> \
-    --path <path-to-app-yaml> \
-    --dest server:default
+ argocd app create netflix-app \
+	--repo https://github.com/lizabethwordp/netflixclone.git \
+	--path netflix-app \
+	--dest-server https://kubernetes.default.svc \
+	--dest-namespace netflix-app \
+	--sync-option CreateNamespace=true \
+	--parameter namespace=netflix-app \
+application 'netflix-app' created
 
 # Sync the application (deploy)
-argocd app sync my-app
+argocd app sync netflix-app
 
 # Monitor deployment status
-argocd app get my-app
+argocd app get netflix-app
