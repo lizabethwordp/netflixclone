415a11677eb777ad38234128e3932a8874588544
lizabethwordp <ajoseanuoluwapo8@gmail.com>
2024-07-01 20:02:09 +0000

    setup app deployment with rancher

--- main.tf
+++ main.tf
@@ -1,18 +1,69 @@
-# Configure the Rancher2 provider to admin
 provider "rancher2" {
-  api_url    = "https://rancher.my-domain.com"
-  access_key = var.rancher2_access_key
-  secret_key = var.rancher2_secret_key
+  api_url  = "https://your-rancher-url.com/v3"
+  token_key = "your-rancher-api-token"
+  token_secret = "your-rancher-api-secret"
 }
 
-resource "aws_instance" "dockerInstance" {
-  cluster_id = "<CLUSTER_ID>"
-  name = "netflixclone_chart"
-  namespace = "cis-operator-system"
-  repo_name = "https://github.com/lizabethwordp/netflixclone/"
-  chart_name = "netflixclone_chart"
-  tags = {
-    Name = "dockerInstance"
-
-  user_data = file("install.sh")
+# Define Kubernetes resources for ArgoCD
+resource "kubernetes_namespace" "argocd_namespace" {
+  metadata {
+    name = "argocd"
+  }
 }
+
+resource "kubernetes_service_account" "argocd_service_account" {
+  metadata {
+    name      = "argocd-service-account"
+    namespace = kubernetes_namespace.argocd_namespace.metadata.0.name
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_cluster_role_binding" {
+  metadata {
+    name = "argocd-cluster-role-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.argocd_service_account.metadata.0.name
+    namespace = kubernetes_namespace.argocd_namespace.metadata.0.name
+  }
+}
+
+resource "kubernetes_deployment" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = kubernetes_namespace.argocd_namespace.metadata.0.name
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = "argocd-server"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "argocd-server"
+        }
+      }
+      spec {
+        service_account_name = kubernetes_service_account.argocd_service_account.metadata.0.name
+        containers {
+          name  = "argocd-server"
+          image = "argoproj/argocd:v2.1.0"
+          ports {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
